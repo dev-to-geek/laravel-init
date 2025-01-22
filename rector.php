5606270f9ee0f26f5595cf955d6cce20beec0d05
mirchaemanuel <mirchaemanuel@gmail.com>
2025-01-22 16:15:32 +0100

    rector config

--- rector.php
+++ rector.php
@@ -9,7 +9,7 @@ return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/src',
         __DIR__.'/config',
-        __DIR__.'/tests',
+        __DIR__.'/tests'
     ])
     ->withSkip([
         AddOverrideAttributeToOverriddenMethodsRector::class,
